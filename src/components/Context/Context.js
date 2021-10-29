85c4ad1337f9b4e772b4e66e752fca8e8f1a6001
Cennet Boran <cennetboran22@gmail.com>
2021-10-29 18:31:20 +0300

    Added filtering action

--- src/components/Context/Context.js
+++ src/components/Context/Context.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext } from "react";
-import { products } from "../../lib/products";
+import { products as mainProductsList } from "../../lib/products";
 import Container from "../Layout/Container/Container";
 import Filter from "../Layout/Filter/Filter";
 import Header from "../Layout/Header/Header";
@@ -8,10 +8,30 @@ export const MainContext = createContext();
 
 const Context = (props) => {
   const [basketItems, setBasketItems] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [filterOptions, setFilterOptions] = useState([]);
 
   useEffect(() => {
+    setProducts(mainProductsList);
     setBasketItems(JSON.parse(localStorage.getItem("basket")) || []);
   }, []);
+
+  useEffect(() => {
+    console.log(filterOptions);
+    if (filterOptions.length > 0) {
+      console.log("2");
+      let filteredProducts = [...mainProductsList];
+      filterOptions.map((option) => {
+        filteredProducts = filteredProducts.filter(
+          (x) => x[option.type] === option[option.type]
+        );
+      });
+      setProducts([...filteredProducts]);
+    } else {
+      console.log("3");
+      setProducts([...mainProductsList]);
+    }
+  }, [filterOptions]);
 
   const addToCart = (item) => {
     let tempCart = [...basketItems];
@@ -33,6 +53,8 @@ const Context = (props) => {
     setBasketItems,
     addToCart,
     removeFromCart,
+    filterOptions,
+    setFilterOptions,
   };
   return (
     <MainContext.Provider value={properties}>
--- src/components/Layout/Filter/Filter.js
+++ src/components/Layout/Filter/Filter.js
@@ -1,29 +1,124 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import { MainContext } from "../../Context/Context";
 import "./Filter.scss";
 
 const Filter = (props) => {
+  const { products, filterOptions, setFilterOptions } = useContext(MainContext);
+  const [colors, setColors] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    setFilterByColor();
+    setFilterByBrand();
+  }, [products]);
+
+  const setFilterByColor = () => {
+    let filteredByColor = [];
+    products.map((item) => {
+      if (filteredByColor.filter((x) => x.color === item.color).length <= 0) {
+        filteredByColor.push({
+          type: "color",
+          color: item.color,
+          count: 1,
+        });
+      } else {
+        filteredByColor.filter((x) => x.color === item.color)[0].count += 1;
+      }
+    });
+    setColors(filteredByColor);
+  };
+
+  const setFilterByBrand = () => {
+    let filteredByBrand = [];
+    products.map((item) => {
+      if (filteredByBrand.filter((x) => x.brand === item.brand).length <= 0) {
+        filteredByBrand.push({
+          type: "brand",
+          brand: item.brand,
+          count: 1,
+        });
+      } else {
+        filteredByBrand.filter((x) => x.brand === item.brand)[0].count += 1;
+      }
+    });
+    setBrands(filteredByBrand);
+  };
+
+  const setFilters = (item) => {
+    let options = [...filterOptions, item];
+    setFilterOptions(options);
+  };
+  const removeFromFilters = (item) => {
+    if (filterOptions.length > 0) {
+      let newFilter = [...filterOptions];
+      let ind = newFilter.findIndex((x) => x[item.type] === item[item.type]);
+      newFilter.splice(ind, 1);
+      console.log(newFilter);
+      setFilterOptions([...newFilter]);
+    }
+  };
+
   return (
     <>
       <div className="filter-wrapper">
         <h4>Renk</h4>
-        <span className="filter-item">Kırmızı</span>
-        <span className="filter-item">Lacivert</span>
-        <span className="filter-item">Sarı</span>
-        <span className="filter-item">Siyah</span>
-        <span className="filter-item">Beyaz</span>
+        {colors.length > 0 &&
+          colors.map((item) => {
+            if (
+              filterOptions.filter((x) => x.color === item.color).length > 0
+            ) {
+              return (
+                <span className="filter-item">
+                  <button
+                    className="remove-filter-button"
+                    onClick={() => removeFromFilters(item)}
+                  >
+                    x
+                  </button>
+                  {item.color}({item.count})
+                </span>
+              );
+            } else {
+              return (
+                <span className="filter-item" onClick={() => setFilters(item)}>
+                  {item.color}({item.count})
+                </span>
+              );
+            }
+          })}
+
         <h4>Sıralama</h4>
         <span className="filter-item">En Düşük Fiyat</span>
         <span className="filter-item">En Yüksek Fiyat</span>
         <span className="filter-item">En Yeniler (A{">"}Z)</span>
         <span className="filter-item">En Yeniler (Z{"<"}A)</span>
+
         <h4>Marka</h4>
-        <span className="filter-item">Samsung</span>
-        <span className="filter-item">Nokia</span>
-        <span className="filter-item">Apple</span>
-        <span className="filter-item">LG</span>
-        <span className="filter-item">Huawei</span>
-        <span className="filter-item">Xiaomi</span>
-        <span className="filter-item">General Mobile</span>
+        {brands.length > 0 &&
+          brands.map((item) => {
+            if (
+              filterOptions.filter((x) => x.brand === item.brand).length > 0
+            ) {
+              return (
+                <span className="filter-item">
+                  <button
+                    className="remove-filter-button"
+                    onClick={() => removeFromFilters(item)}
+                  >
+                    x
+                  </button>
+                  {item.brand}({item.count})
+                </span>
+              );
+            } else {
+              return (
+                <span className="filter-item" onClick={() => setFilters(item)}>
+                  {item.brand}({item.count})
+                </span>
+              );
+            }
+          })}
       </div>
     </>
   );
